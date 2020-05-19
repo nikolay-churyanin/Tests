55ff0012d28f389d6dc38ed1e72395f484a3e430
Nikolay <n.churyanin@yandex.ru>
2020-05-19 17:06:37 +0400

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Nikolay Churyanin on 19.05.2020.
+//  Copyright © 2020 Nikolay Churyanin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
