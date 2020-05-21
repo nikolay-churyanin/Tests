6191ae1511e23bff2a68e17154b49906bbbcb691
Nikolay <n.churyanin@yandex.ru>
2020-05-21 20:34:28 +0400

    Initial Commit

---
+++ Test3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test3
+//
+//  Created by Nikolay Churyanin on 21.05.2020.
+//  Copyright © 2020 Nikolay Churyanin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
